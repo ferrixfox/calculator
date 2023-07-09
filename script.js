0a90e2d2290a14f25add89b5e0316620283eb725
englishle86 <englishle86@gmail.com>
2023-07-09 19:31:15 -0400

    create beginning script file and source it in html file

---
+++ script.js
@@ -0,0 +1,17 @@
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function divide(num1, num2){
+    let quotient = num1 / num2;
+    return quotient.toFixed(7);
+}
+
+function multiply(num1, numb2){
+    product = num1 * num2;
+    return product.toFixed(7)
+}
